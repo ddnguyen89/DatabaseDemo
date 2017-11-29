1192c3f094e76d7493f7c058911011537827f162
eatdabanunu <duynguyen.xz@gmail.com>
2017-11-29 09:31:20 -0800

    Update: 11/29/2017

--- app/src/main/java/nguyen/databasedemo/data/MyDBHandler.java
+++ app/src/main/java/nguyen/databasedemo/data/MyDBHandler.java
@@ -9,7 +9,17 @@ import android.database.sqlite.SQLiteOpenHelper;
  */
 
 public class MyDBHandler extends SQLiteOpenHelper{
-    public MyDBHandler(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+
+    //define database variables
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "contacts.db";
+    public static final String TABLE_CONTACTS = "contacts";
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_FIRSTNAME = "firstName";
+    public static final String COLUMN_LASTTNAME = "lastName";
+
+
+    public MyDBHandler(Context context) {
         super(context, name, factory, version);
     }
 
